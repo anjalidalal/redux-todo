9a11a0c6466bd93c79c5961cd8d1d33162f1147e
anjalidalal <anjalimaria2002@gmail.com>
2022-10-20 17:42:45 +0530

    Todo in redux

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,13 +1,15 @@
-import React, { useReducer, useState } from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addTodo, deleteTodo, editTodo } from "./Action";
 import { nanoid } from "nanoid";
 
 const Todo = () => {
   const [text, setText] = useState("");
+  const [editing, setEditing] = useState(false);
   const dispatch = useDispatch();
   const todoReducer = useSelector((store) => store);
   console.log(todoReducer);
+  let taskContent;
 
   return (
     <div className="main">
@@ -31,76 +33,45 @@ const Todo = () => {
       >
         Add
       </button>
-      <ul>
-        {todoReducer.map((todo) => (
-          <li key={todo.id}>
-            <Task todo={todo} />
-          </li>
-        ))}
-      </ul>
+      {todoReducer.map((todo) => (
+        <div className="list" key={todo.id}>
+          <p key={todo.id}>
+            {editing
+              ? (taskContent = (
+                  <>
+                    <input
+                      type="text"
+                      value={todo.title}
+                      onChange={(e) => {
+                        dispatch(
+                          editTodo({
+                            ...todo,
+                            title: e.target.value,
+                          })
+                        );
+                      }}
+                    />
+                    <button onClick={() => setEditing(false)}>Save</button>
+                  </>
+                ))
+              : (taskContent = (
+                  <>
+                    {todo.title}
+                    <button onClick={() => setEditing(true)}>Edit</button>
+                  </>
+                ))}
+            <button
+              onClick={() => {
+                dispatch(deleteTodo(todo.id));
+              }}
+            >
+              Delete
+            </button>
+          </p>
+        </div>
+      ))}
     </div>
   );
 };
 
-function Task({ todo }) {
-  const [editing, setEditing] = useState(false);
-
-  let taskContent;
-
-  if (editing) {
-    taskContent = (
-      <>
-        <input
-          type="text"
-          value={todo.title}
-          onChange={(e) => {
-            dispatch(
-              editTodo({
-                ...todo,
-                title: e.target.value,
-              })
-            );
-          }}
-        />
-        <button onClick={() => setEditing(false)}>Save</button>
-      </>
-    );
-  } else {
-    taskContent = (
-      <>
-        {todo.title}
-        <button onClick={() => setEditing(true)}>Edit</button>
-      </>
-    );
-  }
-
-  return (
-    <>
-      {taskContent}
-      <button
-        onClick={() => {
-          dispatch(deleteTodo(todo.id));
-        }}
-      >
-        Delete
-      </button>
-    </>
-  );
-}
-
 export default Todo;
-
-{
-  /* <label key={todo.id}>
-  <input type="checkbox" />
-  {todo.title}
-  <button
-    onClick={() => {
-      dispatch(deleteTodo(todo.id));
-    }}
-  >
-    Delete
-  </button>
-  <button>Edit</button>
-</label>; */
-}
